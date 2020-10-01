0d447050aa43de666fad0118a592ef24d88051cc
lukecardita <70244038+lukecardita@users.noreply.github.com>
2020-10-01 12:27:02 -0400

    Lab 4 and Assignment 5
    Updated Lab 4 and assignment 5

--- Assignments/Assignment 5/wet_code.py
+++ Assignments/Assignment 5/wet_code.py
@@ -4,12 +4,12 @@
 # Some things to note. When we have a positive number that gets split up and 85% goes into checking and 15% goes into savings. 
 # All negative numbers gets taken out of the checking account.
 
-
-def saturdays_bank_transactions(transations) -> (float, float):
+def saturdays_bank_transactions(transactions) -> (float, float):
     savings = 1096.25
-    checking = 1590.80
+    checking = 159.80
     pass
-if __name__ == "__main__":
-    transations = [300.00, -50.00, -5.00, -20, 15.72, 2083.93, -1034.00, -420.00, -5.23, -15.93, -72.90]
-    new_balance = saturdays_bank_transactions(transations)
-    print("Your new checking balance is:", '${:.2f}'.format(round(new_balance[0], 2)), "\n", "Your new savings balance is:", '${:.2f}'.format(round(new_balance[1], 2)))
+    if __name__ == "__main__":
+        transactions = [300.00, -50.00, -5.00, -20, 15.72, 2083.93, -1034.00, -420.00, -5.23, -15.93, -72.90]
+        new_balance = saturdays_bank_transactions(transactions)
+        print()
+saturdays_bank_transactions(transations)
