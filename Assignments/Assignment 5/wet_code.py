7181857b0fc14ac4286145e14070bb4dad4e203e
lukecardita <70244038+lukecardita@users.noreply.github.com>
2020-10-27 12:54:41 -0400

    Updates to Assignments 4 and 5
    Updated assignments 4 and 5 to be regraded. Assignment 4 is found at the bottom of lab 3

--- Assignments/Assignment 5/wet_code.py
+++ Assignments/Assignment 5/wet_code.py
@@ -6,10 +6,15 @@
 
 def saturdays_bank_transactions(transactions) -> (float, float):
     savings = 1096.25
-    checking = 159.80
-    pass
-    if __name__ == "__main__":
-        transactions = [300.00, -50.00, -5.00, -20, 15.72, 2083.93, -1034.00, -420.00, -5.23, -15.93, -72.90]
-        new_balance = saturdays_bank_transactions(transactions)
-        print()
-saturdays_bank_transactions(transations)
+    checking = 1590.80
+    for x in transactions:
+        if x >= 0:
+            checking += (x * 0.85)
+            savings += (x * 0.15)
+        else:
+            checking += x
+    return checking, savings
+if __name__ == "__main__":
+    transactions = [300.00, -50.00, -5.00, -20, 15.72, 2083.93, -1034.00, -420.00, -5.23, -15.93, -72.90]
+    new_balance = saturdays_bank_transactions(transactions)
+    print("Your new checking balance is:", '${:.2f}'.format(round(new_balance[0], 2)), "\n", "Your new savings balance is:", '${:.2f}'.format(round(new_balance[1], 2)))
