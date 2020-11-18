61bfbe8870c6d9088a854c59fca3ab288be422dc
lukecardita <70244038+lukecardita@users.noreply.github.com>
2020-11-17 21:07:43 -0500

    Create Calculations Using Calculator.py

---
+++ Assignments/Assignment 9/Calculations Using Calculator.py
@@ -0,0 +1,10 @@
+from Calculator import Calculator
+calc = Calculator()
+
+print(calc.addition(1,7))
+print(calc.subtraction(9,2))
+print(calc.multiplication(6,4))
+print(calc.division(9,3))
+print(calc.exponent(2,6))
+print(calc.square_root(27))
+print(calc.negate(6))
